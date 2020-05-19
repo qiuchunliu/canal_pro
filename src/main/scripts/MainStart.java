fa7ba7e06f27fdd39bcac2293c42b7201abf3184
邱春柳 <qiuchunliu@hzsuidifu.com>
2020-05-19 18:08:52 +0800

    temp

--- src/main/scripts/MainStart.java
+++ src/main/scripts/MainStart.java
@@ -1,13 +1,11 @@
 package main.scripts;
 
 
-import org.dom4j.DocumentException;
 import org.apache.log4j.Logger;
-import java.io.IOException;
 
 public class MainStart {
     private static Logger log = Logger.getLogger(MainStart.class);
-    public static void main(String[] args) throws DocumentException, IOException {
+    public static void main(String[] args) {
         log.info("START THE JOB");
         String conn_str = "mysql|klingon=mycanal:1111@111.231.66.20:3306/canaltobase" +
                 ",mysql|yunxin=canal:1111@192.168.24.11:3306/canaltobase," +
@@ -15,6 +13,6 @@ public class MainStart {
         RunJob runJob = new RunJob("main/resources/schema.xml",
                 "main/resources/config.properties", conn_str);
         runJob.doit();
-        log.info("THE JOB IS DONE");
+        log.info("JOB IS DONE");
     }
 }
